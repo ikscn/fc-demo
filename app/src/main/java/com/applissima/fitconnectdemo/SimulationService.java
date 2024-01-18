275b216d56b30a15201899f3cc55902197dc1c0f
Ilker Kuscan <ilker.kscn@gmail.com>
2024-01-18 16:25:12 +0100

    Update SimulationService.java
    Code cleaning.

--- app/src/main/java/com/applissima/fitconnectdemo/SimulationService.java
+++ app/src/main/java/com/applissima/fitconnectdemo/SimulationService.java
@@ -13,11 +13,9 @@ public class SimulationService {
 
     public static String[] simulationBelts = new String[]
             {
-                    //"10024",
                     "10025",
                     "10030",
                     "10031",
-                    //"10032",
                     "10033",
                     "10034",
                     "10035",
@@ -37,52 +35,4 @@ public class SimulationService {
                     "11024",
                     "11124"
             };
-
-
-    /*public static void generateSimulationFitPersons(int personCount, DatabaseService dbService){
-
-        Realm mRealm = null;
-
-        try {
-
-            mRealm = Realm.getDefaultInstance();
-
-            for (int i = 0; i < personCount; i++) {
-
-                String hrSensorId = simulationBelts[i];
-
-                if (mRealm.where(FitPerson.class).equalTo("hrSensorId", hrSensorId).count() > 0) {
-                    continue;
-                }
-
-                dbService.updateOrAddLocalUserData(hrSensorId, true);
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if(mRealm!=null) {
-                mRealm.closeDialog();
-            }
-        }
-
-    }*/
-
-    /*public static void generateSimulationFwPersons(int personCount, DatabaseService databaseService){
-
-        for(int i=0; i<personCount; i++){
-            databaseService.createInitWork(simulationBelts[i]);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }*/
 }
